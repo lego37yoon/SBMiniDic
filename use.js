8e879bf52497ca44fb1691e79cc63084429c6ed4
윤정민(종이상자) <lego37yoon@outlook.com>
2021-01-17 17:16:20 +0900

    2021.01.17 bug-fix
     - Modify manifest.json to apply "matches" and "js" properties.
     - Check what's wrong in this script and clarify my knowledge about XMLHttpRequest().

--- use.js
+++ use.js
@@ -2,19 +2,17 @@ function showFrame() {
     let userText = window.getSelection().toString().trim();
     let requestDic = new XMLHttpRequest();
     let dicRawText;
-    if (userText != null) {
-        requestDic.open('GET', 'https://suggest-bar.daum.net/suggest', true);
-        requestDic.send('mod=json&code=utf_in_out&enc=utf&id=language&cate=lan&q=' + userText);
+    if (userText !== '') {
+        requestDic.open('GET', 'https://suggest-bar.daum.net/suggest?mod=json&code=utf_in_out&enc=utf&id=language&cate=lan&q=' + userText, true);
+        requestDic.responseType = 'json';
         requestDic.onreadystatechange = function() {
+            console.log('Check State...');
             if(requestDic.readyState === XMLHttpRequest.DONE) {
-                if (requestDic.status === 200) {
-                    dicRawText = JSON.stringify(requestDic.responseText, ['items']);
-                    console.log(dicRawText);
-                }
-            } else {
-
+                console.log(requestDic.response);
             }
-        }
+        };
+        requestDic.send();
+        console.log('Request Sent');
     }
 }
 
