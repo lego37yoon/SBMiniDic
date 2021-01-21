872accba04c442a205fb50fb804074757458edee
윤정민(종이상자) <lego37yoon@outlook.com>
2021-01-21 23:24:00 +0900

    2021.01.21 change to fetch API
     - Result: Failed
     - Test Fetch API

--- use.js
+++ use.js
@@ -1,19 +1,27 @@
 function showFrame() {
     let userText = window.getSelection().toString().trim();
-    let requestDic = new XMLHttpRequest();
+    //let requestDic = new XMLHttpRequest();
     let dicRawText;
-    if (userText !== '') {
+    /* if (userText !== '') {
         requestDic.open('GET', 'https://suggest-bar.daum.net/suggest?mod=json&code=utf_in_out&enc=utf&id=language&cate=lan&q=' + userText, true);
         requestDic.responseType = 'json';
         requestDic.onreadystatechange = function() {
             console.log('Check State...');
             if(requestDic.readyState === XMLHttpRequest.DONE) {
-                console.log(requestDic.response);
+                console.log(requestDic.responseText);
             }
         };
         requestDic.send();
         console.log('Request Sent');
-    }
+    } */
+    fetch('https://suggest-bar.daum.net/suggest?mod=json&code=utf_in_out&enc=utf&id=language&cate=lan&q=let')
+    .then(function(response) {
+        console.log(response);
+        dicRawText = response.json();
+    })
+    .catch(function(error) {
+        console.error(error);
+    });
 }
 
 document.addEventListener("mouseup", showFrame);
