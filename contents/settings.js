4995a772c045d9913481fb1c72665ca736472180
윤정민(종이상자) <lego37yoon@outlook.com>
2021-01-28 14:56:52 +0900

    2021.01.28 #1
    - add name and value at radio button (mode)
    - add mode to settings.js
    - new app id

--- contents/settings.js
+++ contents/settings.js
@@ -1,8 +1,18 @@
 function saveValues() {
+    let appMode = document.getElementsByName('mode');
+    
     browser.storage.sync.set({
         apikey: document.querySelector("#apikey").value,
         dragToFind: document.querySelector("#dragToFind").value,
+        contextToFind: document.querySelector("#contextToFind").value,
     });
+    for (i = 0; i < appMode.length; i++) {
+        if (appMode[i].checked) {
+            browser.storage.sync.set({
+                mode: appMode[i].value
+            })
+        }
+    }
 }
 
 document.querySelector("form").addEventListener("submit", saveValues);
