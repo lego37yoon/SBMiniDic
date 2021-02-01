2508a9b9f158a592e9fd223089711657fed6bf36
윤정민(종이상자) <lego37yoon@outlook.com>
2021-02-01 23:39:28 +0900

    2021.02.01 Test Build
     - [manifest.json] 원활한 디버그를 위해 별도 페이지에서 설정 열리도록 조정
     - init() 함수에서 browser.storage.sync.get으로 저장되어 있는 API 키 불러오지 못하는 오류 수정
     - saveValues() 함수가 API 키를 저장하지 못하는 오류 수정
     - init() 함수가 체크박스를 제어하지 못하는 버그 수정

--- contents/settings.js
+++ contents/settings.js
@@ -1,34 +1,64 @@
 function init() {
+    console.log("init...");
     let savedApiKey = browser.storage.sync.get("apikey");
-    document.getElementById("apikey").value = savedApiKey;
+    savedApiKey.then((res) => {
+        document.getElementById("apikey").value = res.apikey;
+    });
+    console.log("(2021.02.01) no error");
+
     let savedDragToFind = browser.storage.sync.get("dragToFind");
-    let savedContextToFind = browser.storage.sync.get("contextToFind");
-    if (savedDragToFind == "on") {
-        document.getElementById("dragToFind").setAttribute("checked");
-    } else {
-        document.getElementById("dragToFind").removeAttribute("checked");
-    }
-    if (savedContextToFind == "on") {
-        document.getElementById("contextToFind").setAttribute("checked");
-    } else {
-        document.getElementById("contextToFind").removeAttribute("checked");
-    }
+    savedDragToFind.then((res) => {
+        console.log(res.dragToFind);
+        if (res.dragToFind == true) {
+            console.log("nothing to do.");
+        } else {
+            document.getElementById("dragToFind").checked = false;
+        }
+    });
+    console.log("Checked Attribute");
     
+    let savedContextToFind = browser.storage.sync.get("contextToFind");
+    savedContextToFind.then((res) => {
+        console.log(res.contextToFind);
+        if (res.contextToFind == true) {
+            document.getElementById("contextToFind").checked = true;
+        } else {
+            document.getElementById("contextToFind").checked = false;
+        }
+        console.log("Checked Attribute");
+    }); 
 }
 
 function saveValues() {
     let appMode = document.getElementsByName('mode');
+    let dragMode;
+    let contextMode;
+
+    if (document.getElementById("dragToFind").value != "on" && document.getElementById("contextToFind").value != "on") {
+        alert("최소한 하나 이상의 방법을 선택하세요.");
+    } else {
+        if (document.getElementById("dragToFind").value == "on") {
+            dragMode = true;
+        } else {
+            dragMode = false;
+        }
+        if (document.getElementById("contextToFind").value == "on") {
+            contextMode = true;
+        } else {
+            contextMode = false;
+        }    
+    }
     
     browser.storage.sync.set({
-        apikey: document.querySelector("#apikey").value,
-        dragToFind: document.querySelector("#dragToFind").value,
-        contextToFind: document.querySelector("#contextToFind").value,
+        apikey: document.getElementById("apikey").value,
+        dragToFind: dragMode,
+        contextToFind: contextMode
     });
     for (i = 0; i < appMode.length; i++) {
         if (appMode[i].checked) {
             browser.storage.sync.set({
                 mode: appMode[i].value
-            })
+            });
         }
     }
 }
